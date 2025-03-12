9c11109be25db74f03102bfadede77224763857f
Jacob Lee <jacob.lee2419@gmail.com>
2025-03-12 16:49:56 -0500

    Update UI.py

--- UI.py
+++ UI.py
@@ -7,7 +7,36 @@ root = Tk()
 # root window title and dimension
 root.title("Example")
 # Set geometry (widthxheight)
-root.geometry('350x200')
+root.geometry('500x400')
+
+menu = Menu(root)
+item = Menu(menu)
+item.add_command(label='New')
+menu.add_cascade(label='File', menu=item)
+root.config(menu=menu)
+
+# adding a label to the root window
+lbl = Label(root, text = "Enter data")
+lbl.grid()
+
+txt = Entry(root, width=10)
+txt.grid(column =1, row =0)
+
+def clicked():
+    lbl.configure(text = "Data has been entered")
+    newlbl = Label(root, text = "You entered " + txt.get())
+    newlbl.grid(column=1,row=1)
+    txt.delete(0, END)
+    txt.insert(0,"")
+
+
+
+# button widget with red color text
+# inside
+btn = Button(root, text = "Enter" ,
+             fg = "red", command=clicked)
+# set Button grid
+btn.grid(column=2, row=0)
 
 # all widgets will be here
 # Execute Tkinter
