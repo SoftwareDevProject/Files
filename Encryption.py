617fbfa7f20129d80a89184848d40ee789de1d84
Jacob Lee <jacob.lee2419@gmail.com>
2025-03-12 15:43:39 -0500

    Added an entry class

--- Encryption.py
+++ Encryption.py
@@ -1,17 +1,30 @@
+# Importing modules
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 
+# password 
+data = b'password'
 
-data = b'secret data'
-
+# Creation of encryption key
 key = get_random_bytes(16)
+
+# Encryption
 cipher = AES.new(key, AES.MODE_EAX)
 ciphertext, tag = cipher.encrypt_and_digest(data)
 nonce = cipher.nonce
 
 print(ciphertext)
 
+# Decryption
 decryptionCipher = AES.new(key,AES.MODE_EAX, nonce)
 ddata = decryptionCipher.decrypt_and_verify(ciphertext, tag)
 
 print(ddata)
+
+class Entry:
+  def __init__(email, password, key, nonce):
+    self.email = email
+    self.password = password
+    self.key = key
+    self.nonce = nonce
+    
